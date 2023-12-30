c357ba78c5fcba777dc93b16baf96668f82ac639
Greg Donald <gd@darkclear.net>
2023-12-30 10:43:27 -0600

    add fork me

---
+++ src/components/ForkMe.tsx
@@ -0,0 +1,9 @@
+export default function ForkMe() {
+  return (
+    <div className='fork-me'>
+      <a href='https://github.com/gdonald/blackjack-js' target='_blank'>
+        Fork Me On Github!
+      </a>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Game from './components/Game'
+import ForkMe from './components/ForkMe'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Game key='g'></Game>
+    <ForkMe></ForkMe>
   </React.StrictMode>
 )
